37890d2cc4d99a1883b66860677525278f20bbb3
moregorenine <moregorenine@gmail.com>
2020-01-03 22:38:45 +0900

    Init Project

---
+++ src/main/java/com/w4springrain/springbootwhiteship/SpringbootWhiteshipApplication.java
@@ -0,0 +1,13 @@
+package com.w4springrain.springbootwhiteship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWhiteshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWhiteshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w4springrain/springbootwhiteship/SpringbootWhiteshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w4springrain.springbootwhiteship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWhiteshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
